683d6e135a658196946f6499bb4a99dd03b47883
Мельник Анастасия <79141800166@yandex.ru>
2025-06-18 00:05:17 +1000

    change to axios

--- src/layouts/model/whoamiService.ts
+++ src/layouts/model/whoamiService.ts
@@ -1,16 +1,18 @@
 import type { whoami } from "./model";
+import axios from "axios";
 
 export async function fetchWhoami(): Promise<whoami> {
   try {
     const baseUrl = import.meta.env.VITE_BACK_ADDRESS;
     const accessToken = localStorage.getItem("accessToken");
+    console.log("baseUrl", baseUrl);
 
     if (!accessToken) {
       handleLogout();
       throw new Error("Access token not found in localStorage");
     }
 
-    const response = await fetch(`${baseUrl}api/v1/whoami`, {
+    const response = await axios.get(`${baseUrl}api/v1/whoami`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
@@ -21,14 +23,15 @@ export async function fetchWhoami(): Promise<whoami> {
       throw new Error("Unauthorized access");
     }
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data: whoami = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
     console.error("Ошибка при получении данных whoami:", error);
+    if (
+      axios.isAxiosError(error) &&
+      (error.response?.status === 401 || error.response?.status === 403)
+    ) {
+      handleLogout();
+    }
     throw error;
   }
 }
